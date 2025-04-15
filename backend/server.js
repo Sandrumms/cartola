3bbd92d7f56dad405fcd87dd9ce60e4800dc2c40
Sandro Paula <sandrosanpaula@gmail.com>
2025-04-15 10:33:14 -0300

    Ajustando server.js

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
 
 server.use(middlewares);
 server.use(router);
